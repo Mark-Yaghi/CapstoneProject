93aadf1634e864e044ac7bfa02cd32909ea7a19a
Arv <pandit.arv@gmail.com>
2022-10-14 21:23:24 -0600

    navbar setup for the front end page; excuding login page

---
+++ capstone_HRAgency/ClientApp/src/components/Chat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+	return (
+		<div>
+			<h1>Let's Chat</h1>
+		</div>
+	);
+};
+
+export default Chat;
---
+++ capstone_HRAgency/ClientApp/src/components/ExpertSolution.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExpertSolution = () => {
+	return (
+		<div>
+			<h1>ExpertSolution</h1>
+		</div>
+	);
+};
+
+export default ExpertSolution;
