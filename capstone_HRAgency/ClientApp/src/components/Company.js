7594c1edd3de56fe1b69e8ef3eba77f21b2cfc2a
Corey Woodcox <ctwoodcox@outlook.com>
2022-10-20 14:41:27 -0600

    Resolved Merge conflicts and errors in Company.js & _LoginPartial.cshtml from the latest mark/merge. Everything is working.

--- capstone_HRAgency/ClientApp/src/components/Company.js
+++ capstone_HRAgency/ClientApp/src/components/Company.js
@@ -1,8 +1,4 @@
 ﻿import React, { Component, useState, useEffect, useRef } from 'react';
-import authService from './api-authorization/AuthorizeService';
-import { useNavigate, navigate } from 'react-router-dom';
-//import '../../src/custom.css';
-import React, { Component } from "react";
 import authService from "./api-authorization/AuthorizeService";
 import { NavLink } from "react-router-dom";
 import { LoginMenu } from "./api-authorization/LoginMenu";
@@ -109,83 +105,6 @@ export class Company extends Component
             
         );
      }
-export class Company extends Component {
-	static displayName = Company.name;
-
-	constructor(props) {
-		super(props);
-		this.state = { isAuthenticated: false, user: null, loading: false, Companies: [], CompanyID: "", CompanyName: "", Address: "", Phone: "", CPFirstName: "", CPLastName: "", CPEMail: "", StartDate: "", EndDate: "", SubscriptionStatus: "", CompanyCount: "" };
-		// this.incrementCounter = this.incrementCounter.bind(this);
-		this.populateRoles();
-		this.populateCount();
-	}
-
-	async populateRoles() {
-		const token = await authService.getAccessToken();
-		console.log(token);
-		const responseList = await fetch("company/list", {
-			headers: !token ? {} : { Authorization: `Bearer ${token}` }, //Admin
-		});
-		console.log(responseList);
-		if (responseList.ok) {
-			const dataList = await responseList.json();
-			console.log(dataList);
-			this.setState({ Companies: dataList, loading: false });
-			// console.log("Company ID Number" + companies.CompanyID + " Company Name: " + companies.CompanyName);
-		} else {
-			console.log(await responseList.text());
-		}
-	}
-	async populateCount() {
-		const token = await authService.getAccessToken();
-		const responseCount = await fetch("company/count", {
-			headers: !token ? {} : { Authorization: `Bearer ${token}` }, //Admin
-		});
-		const dataCount = await responseCount.json();
-		this.setState({ CompanyCount: dataCount, loading: false });
-		console.log("This is the data count; the number of companies: " + { dataCount });
-	}
-
-	static renderCompaniesTable(CompanyList) {
-		return (
-			<table className="table table-striped" aria-labelledby="tabelLabel">
-				<thead>
-					<tr>
-						<th>Company Name</th>
-						<th>Address</th>
-						<th>Phone</th>
-						<th>CP First Name</th>
-						<th>CP Last Name</th>
-						<th>CP Email</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{CompanyList.map((Companies) => (
-						<tr key={Companies.companyID}>
-							<td>{Companies.companyName}</td>
-							<td>{Companies.address}</td>
-							<td>{Companies.phone}</td>
-							<td>{Companies.cpFirstName}</td>
-							<td>{Companies.cpLastName}</td>
-							<td>{Companies.cpEmail}</td>
-							<td>
-								<button
-									className="but-general but-col-sec"
-									onClick={(() => {
-										this.setState({ loading: true });
-										this.fireMessage();
-									}).bind(this)}
-								>
-									Select
-								</button>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		);
-	}
 
 
     render()
@@ -231,7 +150,7 @@ export class Company extends Component {
             </div>
         );
     }
-}
+
 	render() {
 		let contents = this.state.loading ? (
 			<p>
