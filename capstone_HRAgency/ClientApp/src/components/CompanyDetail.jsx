780d72e8c46e760aedf15eaf8be20f765a565959
Arv <pandit.arv@gmail.com>
2022-10-20 19:12:12 -0600

    destructed the company details

--- capstone_HRAgency/ClientApp/src/components/CompanyDetail.jsx
+++ capstone_HRAgency/ClientApp/src/components/CompanyDetail.jsx
@@ -3,8 +3,9 @@ import { useParams } from "react-router-dom";
 import authService from "./api-authorization/AuthorizeService";
 
 const CompanyDetail = () => {
-	const [singleCompnayDetail, setSingleCompanyDetail] = useState(null);
+	const [singleCompnayDetail, setSingleCompanyDetail] = useState([]);
 	const { companyID } = useParams();
+	const { address, companyName, cpEmail, cpFirstName, cpLastName, startDate, endDate, phone } = singleCompnayDetail;
 	console.log(singleCompnayDetail);
 	useEffect(() => {
 		const populateRoles = async () => {
@@ -26,6 +27,33 @@ const CompanyDetail = () => {
 	return (
 		<div>
 			<h1>CompanyDetail:- {companyID}</h1>
+			<div className="flex-center">
+				<ul>
+					<li>Company Name</li>
+					<li>Address</li>
+					<li>Phone No.</li>
+					<li>Email</li>
+					<li>Contact Person</li>
+					<li>Start Date</li>
+					<li>End Date</li>
+					<li>Subscription Status</li>
+					<li>Type of package</li>
+					<li>Permission Level</li>
+				</ul>
+
+				<ul>
+					<li>{companyName}</li>
+					<li>{address}</li>
+					<li>{phone}</li>
+					<li>{cpEmail}</li>
+					<li>{`${cpFirstName} ${cpLastName}`}</li>
+					<li>{startDate}</li>
+					<li>{endDate}</li>
+					<li>{singleCompnayDetail.subscriptionStatus.toString()}</li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
 		</div>
 	);
 };
