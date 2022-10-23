e7c4c06e81414c1b73bd7af0dd3ba234595f612c
Mark Yaghi <markyaghi@hotmail.com>
2022-10-23 17:17:27 -0600

    Removed edit client from drop down menu; added temporary register button.

--- capstone_HRAgency/ClientApp/src/components/Navbar/NavMenu.js
+++ capstone_HRAgency/ClientApp/src/components/Navbar/NavMenu.js
@@ -23,7 +23,6 @@ export const NavMenu = () => {
 									<a href='/'>Appreciations</a>
 									<a href='/fileupload'>Card Upload</a>
 									<a href='/addClient'>Add New Client</a>
-									<a href='/editClient'>Edit Client</a>
 									<NavLink to="/company">Client Info</NavLink>
 									<a href='/authentication/register'>Register</a>
 									<LoginMenu></LoginMenu>
