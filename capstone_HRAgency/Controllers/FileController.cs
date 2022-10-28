be57dc020829bd99ff527d6c12e9c509a674f455
Arv <pandit.arv@gmail.com>
2022-10-28 04:15:48 -0600

    Title casing the card name;

--- capstone_HRAgency/Controllers/FileController.cs
+++ capstone_HRAgency/Controllers/FileController.cs
@@ -3,42 +3,44 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 
 namespace capstone_HRAgency.Controllers
 {
-  [Route("api/[Controller]")]
-  [ApiController]
-  public class FileController : Controller
-  {
-
-    [HttpPost]
-    public ActionResult Post([FromForm] FileModel file)
+    [Route( "api/[Controller]" )]
+    [ApiController]
+    public class FileController : Controller
     {
-      try
-      {
 
-        string imageName = Path.GetFileNameWithoutExtension(file.FileName);
-
-        imageName = imageName + "-" + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(file.FileName);
-
-        string path = Path.Combine(Directory.GetCurrentDirectory(), "ClientApp/public/ImagesUpload", imageName);
-
-        using (Stream stream = new FileStream(path, FileMode.Create))
+        [HttpPost]
+        public ActionResult Post( [FromForm] FileModel file )
         {
-          file.FormFile.CopyTo(stream);
+            try
+            {
+                TextInfo CardName = CultureInfo.CurrentCulture.TextInfo;
+
+                string imageName = Path.GetFileNameWithoutExtension( CardName.ToTitleCase( file.FileName ) ).Replace( " ", "" );
+
+                imageName = imageName + "-" + DateTime.Now.ToString( "yymmssfff" ) + Path.GetExtension( file.FileName );
+
+                string path = Path.Combine( Directory.GetCurrentDirectory(), "ClientApp/public/ImagesUpload", imageName );
+
+                using ( Stream stream = new FileStream( path, FileMode.Create ) )
+                {
+                    file.FormFile.CopyTo( stream );
+                }
+
+                return StatusCode( StatusCodes.Status201Created );
+
+            }
+            catch ( Exception )
+            {
+                return StatusCode( StatusCodes.Status500InternalServerError );
+            }
         }
 
-        return StatusCode(StatusCodes.Status201Created);
-
-      }
-      catch (Exception)
-      {
-        return StatusCode(StatusCodes.Status500InternalServerError);
-      }
     }
-
-  }
 }
 
 
